d1126e6ddc6540d5c448d9e2700b069b863fb288
Arpita <arpita.mazumder02@gmail.com>
2019-07-12 10:53:11 +0530

    submission

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,10 +1,19 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanNameAware , BeanFactoryAware {
 
 
     //object of actor
-    Actor actor;
+    private Actor actor;
+    private ApplicationContext context= null;
+    private BeanFactory beanFactory;
 
     public Movie (){
 
@@ -24,5 +33,25 @@ public class Movie {
         System.out.println("The name of the actor is "+ actor.getName()+
                 "\nThe gender of the actor is :"+actor.getGender()+
                 "\nThe age of the actor is :"+actor.getAge());
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.context =context;
+        System.out.println("ApplicationContextAware implemented");
+
+    }
+    //BeanNameAware
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println("Bean Name is : "+ beanName);
+
+    }
+
+    //
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+        System.out.println("BeanFactoryAware implemented");
     }
 }
